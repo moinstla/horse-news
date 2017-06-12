20457e48d6c78aefc454f8279ce939d814e1c4da
Monique St.Laurent and Tanner Eustice <monique+tanner@>
2017-06-12 16:43:12 -0700

    the final

--- src/app/mock-posts.ts
+++ src/app/mock-posts.ts
@@ -1,10 +1,10 @@
 import { Post } from './post.model';
 
 export const POSTS: Post[] = [
-   new Post("Billy the black stallion", "Tail is stuck, please help.", "https://jasongarner.com/wp-content/uploads/2015/02/black-stallion.jpg", "horse-personals", 1),
-   new Post("Billy the black stallion", "Tail is stuck, please help.", "http://www.heroviral.com/wp-content/uploads/2015/10/24.png", "horse-personals", 2),
-   new Post("Billy the black stallion", "Tail is stuck, please help.", "http://www.heroviral.com/wp-content/uploads/2015/10/210.png", "horse-personals", 3),
-   new Post("Sally the silver stallion", "My friends say I'm getting a big head, please help.", "http://875357559f655c0fd9842374.eventingnation.netdna-cdn.com/wp-content/uploads/2012/09/enhanced-buzz-23002-1347987098-20.jpg", "horse-advice", 4),
-   new Post("Sally the silver stallion", "There's a giant spider crawling towards me. I can't move, please help.", "http://www.heroviral.com/wp-content/uploads/2015/10/25.png", "horse-advice", 5),
-   new Post("Sally the silver stallion", "I keep accidentally knocking people down and trampling them, please help.", "http://www.heroviral.com/wp-content/uploads/2015/10/26.png", "horse-advice", 6),
+   new Post("Billy the black stallion", "Is there a mare out there who can tame me? I doubt it, but go ahead and try", "https://jasongarner.com/wp-content/uploads/2015/02/black-stallion.jpg", "horse-personals", 1),
+   new Post("Lance, mid-transition/level-1 horse wizard", "Looking for someone who understands me", "http://images.buycostumes.com/mgen/merchandiser/two-man-horse-adult-costume-bc-802966.jpg?zm=1600,1600,1,0,0", "horse-personals", 2),
+   new Post("Uma the Cremello Tennessee Walking Horse", "I'm too beautiful to walk alone.", "http://www.heroviral.com/wp-content/uploads/2015/10/210.png", "horse-personals", 3),
+   new Post("Dingle the horse?", "Other horses keep looking at me like there's something wrong with me? Am I just being paranoid?", "http://875357559f655c0fd9842374.eventingnation.netdna-cdn.com/wp-content/uploads/2012/09/enhanced-buzz-23002-1347987098-20.jpg", "horse-advice", 4),
+   new Post("Barry the Chocolate Silver Dapple Pinto", "There's a giant spider crawling towards me. I can't move, please help.", "http://www.heroviral.com/wp-content/uploads/2015/10/25.png", "horse-advice", 5),
+   new Post("Lars the the Friesian Gypsy Vanner Crossbreed", "I keep accidentally knocking people down and trampling them, please help.", "http://www.heroviral.com/wp-content/uploads/2015/10/26.png", "horse-advice", 6),
  ];
